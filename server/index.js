ff62626bad1480131c941431b594c4ff1d175a5d
balaji-n-k <116717460+Balaji-213@users.noreply.github.com>
2024-09-14 20:12:23 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -36,7 +36,9 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: 'https://sociopedia-gamma-jade.vercel.app'
+}));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 // app.use(cookieSession({
